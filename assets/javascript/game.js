525ea87f7460cbcc468df60b7c6d23592d95ab52
Tyler Winters <tylerwintersdev@gmail.com>
2022-04-20 20:36:56 -0700

    made additions to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,93 @@
-
-
 //create an array of the alphabet
-var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var randomLetter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guessesSoFar = []
+var guessesSoFar = [];
 
 var playerWins = document.getElementById("playerWins");
 var playerLosses = document.getElementById("playerLosses");
 var guessesRem = document.getElementById("guessesRem");
 var guessesUsed = document.getElementById("guessesUsed");
 
-var computerChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+var computerChoice =
+  randomLetter[Math.floor(Math.random() * randomLetter.length)];
 
-var reset =function(){
-    guessesLeft = 10;
-    guessesSoFar = [];
-    computerChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
-
-}
+var reset = function () {
+  guessesLeft = 10;
+  guessesSoFar = [];
+  computerChoice =
+    randomLetter[Math.floor(Math.random() * randomLetter.length)];
+};
 var hasGameStarted = false;
 
+document.getElementById("hide").style.display = "none";
 
-document.getElementById('hide').style.display='none';
+document.onkeyup = function (event) {
+  if (hasGameStarted) {
+    var playerChoice = event.key;
+    var computerChoice =
+      randomLetter[Math.floor(Math.random() * randomLetter.length)];
 
-document.onkeyup = function(event) {
-        
-    if(hasGameStarted){
+    guessesSoFar.push(playerChoice);
 
-            var playerChoice = event.key;
-            var computerChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
-        
-            guessesSoFar.push(playerChoice);
-
-            if (playerChoice === computerChoice) {
-            wins++;
-        }
-        else {
-            guessesLeft--;
-        }
-        if (guessesLeft===0){
-            losses++;
-            reset();
-        }
-        
-        playerWins.textContent = " " + wins;
-        playerLosses.textContent = " " + losses;
-        guessesRem.textContent = ' ' + guessesLeft;
-        guessesUsed.textContent = " " + guessesSoFar
-        
+    if (playerChoice === computerChoice) {
+      wins++;
+      alert("You won! You're psychic skills are improving.");
+      reset();
+    } else {
+      guessesLeft--;
     }
-    else {
-        document.getElementById('hide').style.display='block';
-        hasGameStarted = true;
+    if (guessesLeft === 0) {
+      losses++;
+      alert("You lost. Clear your mind and focus.");
+      reset();
     }
-}
 
-//TODO: computer choice set to random choice from array (randomLetter)
-//TODO: user key stroke needs to check with computer random choice
-//TODO: if user key stroke matches random choice, log in Wins
-//TODO: update Wins html
-//TODO: if user key stroke doesn't match random choice, log in Losses
-//TODO: update Losses html
-//TODO: generate set number of guesses available before win or loss record
-//TODO: generate data displaying how many guesses have been used
+    playerWins.textContent = " " + wins;
+    playerLosses.textContent = " " + losses;
+    guessesRem.textContent = " " + guessesLeft;
+    guessesUsed.textContent = " " + guessesSoFar;
+  } else {
+    document.getElementById("hide").style.display = "block";
+    document.getElementById("hide-test").style.display = "none";
+    hasGameStarted = true;
+  }
+};
+
+//: computer choice set to random choice from array (randomLetter)
+//: user key stroke needs to check with computer random choice
+//: if user key stroke matches random choice, log in Wins
+//: update Wins html
+//: if user key stroke doesn't match random choice, log in Losses
+//: update Losses html
+//: generate set number of guesses available before win or loss record
+//: generate data displaying how many guesses have been used
